c8d1c6d24f76e7434ca04099bc86bb876ed3971b
codetheuri <theurij113@gmail.com>
2025-05-12 21:06:18 +0300

    week1 go

---
+++ week2/basic_error_handling.go
@@ -0,0 +1,19 @@
+package main
+import ("fmt")
+
+func main() {
+  var student1 string = "John" //type is string
+  var student2 = "Jane" //type is inferred
+  x := 2 //type is inferred
+  var a string
+  var b int
+  var c bool
+
+  fmt.Println(student1)
+  fmt.Println(student2)
+  fmt.Println(x)
+  fmt.Println(a)
+  fmt.Println(b)
+  fmt.Println(c)
+}
+// Thursday: Error Types, Error Behaviors
