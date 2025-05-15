5712c552bdcc89bfaf62484794efb00de1ec9868
codetheuri <theurij113@gmail.com>
2025-05-15 22:56:19 +0300

    updates go

--- week2/basic_error_handling.go
+++ week2/basic_error_handling.go
@@ -4,10 +4,15 @@ import ("fmt")
 func main() {
   var student1 string = "John" //type is string
   var student2 = "Jane" //type is inferred
-  x := 2 //type is inferred
+  // x := 2 //type is inferred
   var a string
+  var x int = 2
   var b int
+  var y = "yes"
   var c bool
+  if x > 0 {
+    fmt.Println(y)
+}
 
   fmt.Println(student1)
   fmt.Println(student2)
